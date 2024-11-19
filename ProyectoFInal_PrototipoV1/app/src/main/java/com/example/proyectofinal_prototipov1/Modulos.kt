9d1b3ce61d5076d28363cfa8b4ddc607b30a955b
LauraDaniFlores <laura.dani.flores@gmail.com>
2024-11-18 21:32:22 -0600

    Errores solucionados

--- ProyectoFInal_PrototipoV1/app/src/main/java/com/example/proyectofinal_prototipov1/Modulos.kt
+++ ProyectoFInal_PrototipoV1/app/src/main/java/com/example/proyectofinal_prototipov1/Modulos.kt
@@ -11,18 +11,51 @@ class Modulos : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.modulos)
+
         var continentes: ImageButton = findViewById(R.id.continentes)
+        var oceanos: ImageButton = findViewById(R.id.oceanos)
+        var aryan: ImageButton = findViewById(R.id.aryan)
+        var mexicoEsCap: ImageButton = findViewById(R.id.mexicoEsCap)
+        var mexicoEs: ImageButton = findViewById(R.id.mexicoEs)
+        var america: ImageButton = findViewById(R.id.america)
+        var Asia: ImageButton = findViewById(R.id.Asia)
 
         continentes.setOnClickListener(evento)
+        oceanos.setOnClickListener(evento)
+        aryan.setOnClickListener(evento)
+        mexicoEsCap.setOnClickListener(evento)
+        mexicoEs.setOnClickListener(evento)
+        america.setOnClickListener(evento)
+        Asia.setOnClickListener(evento)
     }
     private val evento = View.OnClickListener { v ->
-        Toast.makeText(applicationContext, "Click botón continentes", Toast.LENGTH_SHORT)
-            .show()
-        startActivity(intent)
         val i: Intent = Intent(
             this@Modulos,
-            ContinentesInter::class.java
+            ModulosIntermedio::class.java
         )
+        when (v.getId()) {
+            R.id.continentes -> {
+                i.putExtra("tiponivel", "0")
+            }
+            R.id.oceanos -> {
+                i.putExtra("tiponivel", "1")
+            }
+            R.id.aryan -> {
+                i.putExtra("tiponivel", "2")
+            }
+            R.id.mexicoEsCap -> {
+                i.putExtra("tiponivel", "3")
+            }
+            R.id.mexicoEs -> {
+                i.putExtra("tiponivel", "4")
+            }
+            R.id.america -> {
+                i.putExtra("tiponivel", "5")
+            }
+            R.id.Asia -> {
+                i.putExtra("tiponivel", "6")
+            }
+        }
         startActivity(i)
     }
 }
